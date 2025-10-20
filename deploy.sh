e907af01b058eb3462f9f2444df4ba6f2e8fd87c
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-10-20 14:34:14 +0100

    Encrypt messages on the SNS topic

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ STACK_OUTPUTS=$(aws cloudformation describe-stacks --stack-name dynamo-alerts-pr
 TABLE_STREAM_ARN=$(echo $STACK_OUTPUTS | jq -r '.[] | select(.OutputKey=="TableStreamArn") | .OutputValue')
 TOPIC_ARN=$(echo $STACK_OUTPUTS | jq -r '.[] | select(.OutputKey=="TopicArn") | .OutputValue')
 TABLE_NAME=$(echo $STACK_OUTPUTS | jq -r '.[] | select(.OutputKey=="TableName") | .OutputValue')
+ENCRYPTION_KEY_ARN=$(echo $STACK_OUTPUTS | jq -r '.[] | select(.OutputKey=="EncryptionKeyArn") | .OutputValue')
 
 # Deploy alerter stack
-sam deploy --template-file infra/alerter.yaml --stack-name dynamo-alerts-alerter --capabilities CAPABILITY_IAM --parameter-overrides TableStreamArn=$TABLE_STREAM_ARN TopicArn=$TOPIC_ARN TableName=$TABLE_NAME
+sam deploy --template-file infra/alerter.yaml --stack-name dynamo-alerts-alerter --capabilities CAPABILITY_IAM --parameter-overrides TableStreamArn=$TABLE_STREAM_ARN TopicArn=$TOPIC_ARN TableName=$TABLE_NAME EncryptionKeyArn=$ENCRYPTION_KEY_ARN
